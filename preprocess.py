ba324798e2f98d8945e8ebc07f6791a0a2256a7d
danielpadkins <danielpadk@gmail.com>
2020-12-30 23:46:55 -0500

    tokenize

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,17 @@
 from transformers import GPT2Tokenizer
 from datasets import load_dataset
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 
 def prepare_data():
     dataset = load_dataset("amazon_reviews_multi", "en")
+    dataset = dataset["train"].map(tokenize)
     return dataset
+
+def tokenize(item):
+    text = item['review_body']
+    return tokenizer.tokenize(text, padding=True, truncation=True)
+
 
 if __name__ == "__main__":
     print(prepare_data())
