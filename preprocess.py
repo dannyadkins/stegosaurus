cb7a958445c3a624c573e212ba1329b51ab35d78
danielpadkins <danielpadk@gmail.com>
2020-12-31 00:13:57 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,15 +2,15 @@ from transformers import GPT2Tokenizer
 from datasets import load_dataset
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-
 def prepare_data():
     dataset = load_dataset("aeslc", "en")
+    print(dataset["train"][0]["email_body"])
     dataset = dataset["train"].map(tokenize)
     return dataset
 
 def tokenize(item):
     text = item['email_body']
-    tokenized = tokenizer("<|txt|> " + text + " <|cph|> ")['input_ids']
+    tokenized = tokenizer.tokenize("<|txt|> " + text + " <|cph|> ")['input_ids']
     return {"tokens": tokenized}
 
 
